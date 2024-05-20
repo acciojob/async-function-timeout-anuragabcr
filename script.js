3d7a9997a8ab687a563f003c35fae11767ed3dbe
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-20 13:06:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn")
+btn.addEventListener("click", async () => {
+	let text = document.getElementById("text").value
+	let delayInput = parseInt(document.getElementById("delay").value)
+	let output = document.getElementById("output")
+	await new Promise(resolve => setTimeout(resolve, delayInput))
+	output.innerHTML = text
+})
